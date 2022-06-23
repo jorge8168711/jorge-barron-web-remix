07359bf680f8fe457a848425afd0e6011241912d
Jorge Barron <jorge.barron@prontohousing.com>
2022-06-22 22:10:38 -0500

    Initial commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,8 @@
+
+export default function Index() {
+  return (
+    <div className='bg-stone-800 h-screen flex'>
+      <img className='w-40 m-auto' src="img/logo.svg" alt="Jorge [Barron]" />
+    </div>
+  );
+}
